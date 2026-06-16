e69f3aaf96fbc4431be6cf581ab88f82458b3f30
shahnoza <sboyitova@gmail.com>
2026-06-16 22:25:16 +0300

    Hw_2.py

--- Hw_2.py
+++ Hw_2.py
@@ -1,18 +1,20 @@
 import streamlit as st
 
-fiyat=int(st.text_input("Lütfen gelirinizi giriniz:"))
+fiyat=st.text_input("Lütfen gelirinizi giriniz:")
+fiyat=float(fiyat)
 vergi=fiyat*0.15
-vergi1=fiyat*0.1525
-vergi2=fiyat*0.195
-vergi3=fiyat*0.27125
-vergi4=fiyat*0.3217592593
-if fiyat in range(0,190000):
-  st.write(vergi)
-elif fiyat in range(190001,400000):
-  st.write(vergi1)
-elif fiyat in range(400001,1500000):
-  st.write(vergi2)
-elif fiyat in range(1500001,5300000):
-  st.write(vergi3)
-elif fiyat>=5300001.00:
-  st.write(vergi4)
+vergi1= (190000 * 0.15) + ((fiyat- 190000) * 0.20)
+vergi2=((190000 * 0.15)+ (210000 * 0.20)+ ((fiyat - 400000) * 0.27))
+vergi3=((190000 * 0.15)+ (210000 * 0.20)+ (1100000 * 0.27)+ ((fiyat - 1500000) * 0.35))
+vergi4=((190000 * 0.15)+ (210000 * 0.20)+ (1100000 * 0.27)+ (3800000 * 0.35)+ ((fiyat - 5300000) * 0.40))
+
+if fiyat <=190000:
+    st.write(vergi)
+elif fiyat <=400000:
+    st.write(vergi1)
+elif fiyat <=1500000:
+    st.write(vergi2)
+elif fiyat <=5300000:
+    st.write(vergi3)
+else:
+    st.write(vergi4)
